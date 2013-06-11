feddf86d51f818541c7cfe327293fa652d694d84
Matias Boero <boeromatias@gmail.com>
2013-06-11 20:46:11 -0300

    Pantalla de bienvenida
    Agregadooo

---
+++ ALFAMA/Pantalla_de_bievenida.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Pantalla_de_bievenida
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bt_siguiente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido a FrbaBus!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(159, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(306, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Haga clic en el boton siguiente para continuar";
+            // 
+            // bt_siguiente
+            // 
+            this.bt_siguiente.Location = new System.Drawing.Point(538, 231);
+            this.bt_siguiente.Name = "bt_siguiente";
+            this.bt_siguiente.Size = new System.Drawing.Size(75, 23);
+            this.bt_siguiente.TabIndex = 2;
+            this.bt_siguiente.Text = "Siguiente";
+            this.bt_siguiente.UseVisualStyleBackColor = true;
+            this.bt_siguiente.Click += new System.EventHandler(this.bt_siguiente_Click);
+            // 
+            // Pantalla_de_bievenida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 266);
+            this.Controls.Add(this.bt_siguiente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Pantalla_de_bievenida";
+            this.Text = "ALFAMA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bt_siguiente;
+    }
+}
---
+++ ALFAMA/Pantalla_de_bievenida.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Pantalla_de_bievenida : Form
+    {
+        public Pantalla_de_bievenida()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_siguiente_Click(object sender, EventArgs e)
+        {
+            Login p = new Login();/*Al desloguearse volvemos a login*/
+            p.Show();
+            this.Hide();
+        }
+    }
+}
